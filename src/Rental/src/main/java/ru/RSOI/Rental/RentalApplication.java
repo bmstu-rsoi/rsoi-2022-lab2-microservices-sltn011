6a1128f4753093a0b29baf0c6206b24398ffafcc
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-10-16 12:27:21 +0300

    Projects init

---
+++ src/Rental/src/main/java/ru/RSOI/Rental/RentalApplication.java
@@ -0,0 +1,13 @@
+package ru.RSOI.Rental;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RentalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RentalApplication.class, args);
+	}
+
+}
---
+++ src/Rental/src/test/java/ru/RSOI/Rental/RentalApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.RSOI.Rental;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
