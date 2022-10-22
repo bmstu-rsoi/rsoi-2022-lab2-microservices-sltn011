4e1cf27c3d02e262eb606850b1385b053dd71f50
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-10-22 22:21:48 +0300

    Setting up docker

--- postgres/20-create-databases.sh
+++ postgres/20-create-databases.sh
@@ -2,7 +2,7 @@
 set -e
 
 # TODO для создания баз прописать свой вариант
-export VARIANT="<variant>"
+export VARIANT="v3"
 export SCRIPT_PATH=/docker-entrypoint-initdb.d/
 export PGPASSWORD=postgres
 psql -f "$SCRIPT_PATH/scripts/db-$VARIANT.sql"
