fadf8a52762e9bbe428f11b2904ac39ef083f566
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-10-23 18:27:44 +0300

    added car to table

--- init.sql
+++ init.sql
@@ -33,3 +33,7 @@ CREATE TABLE payment
         CHECK (status IN ('PAID', 'CANCELED')),
     price       INT         NOT NULL
 );
+
+INSERT INTO public.cars(
+	id, car_uid, brand, model, registration_number, power, price, type, availability)
+	VALUES (1, '109b42f3-198d-4c89-9276-a7520a7120ab', 'Mercedes Benz', 'GLA 250', 'ЛО777Х799', 249, 3500, 'SEDAN', true);
