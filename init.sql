164ed3747343a32266ed7374eefbf201c4105ffb
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-10-23 18:21:53 +0300

    aboba db

---
+++ init.sql
@@ -0,0 +1,35 @@
+CREATE TABLE public.cars
+(
+    id                  SERIAL PRIMARY KEY,
+    car_uid             uuid UNIQUE NOT NULL,
+    brand               VARCHAR(80) NOT NULL,
+    model               VARCHAR(80) NOT NULL,
+    registration_number VARCHAR(20) NOT NULL,
+    power               INT,
+    price               INT         NOT NULL,
+    type                VARCHAR(20)
+        CHECK (type IN ('SEDAN', 'SUV', 'MINIVAN', 'ROADSTER')),
+    availability        BOOLEAN     NOT NULL
+);
+
+CREATE TABLE public.rental
+(
+    id          SERIAL PRIMARY KEY,
+    rental_uid  uuid UNIQUE              NOT NULL,
+    username    VARCHAR(80)              NOT NULL,
+    payment_uid uuid                     NOT NULL,
+    car_uid     uuid                     NOT NULL,
+    date_from   TIMESTAMP WITH TIME ZONE NOT NULL,
+    date_to     TIMESTAMP WITH TIME ZONE NOT NULL,
+    status      VARCHAR(20)              NOT NULL
+        CHECK (status IN ('IN_PROGRESS', 'FINISHED', 'CANCELED'))
+);
+
+CREATE TABLE payment
+(
+    id          SERIAL PRIMARY KEY,
+    payment_uid uuid        NOT NULL,
+    status      VARCHAR(20) NOT NULL
+        CHECK (status IN ('PAID', 'CANCELED')),
+    price       INT         NOT NULL
+);
