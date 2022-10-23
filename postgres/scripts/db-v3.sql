8b9582c70208a49f36f75d4781c54a184d565190
sltn011 <61147580+sltn011@users.noreply.github.com>
2022-10-23 03:36:00 +0300

    Update db-v3.sql

--- postgres/scripts/db-v3.sql
+++ postgres/scripts/db-v3.sql
@@ -6,3 +6,7 @@ GRANT ALL PRIVILEGES ON DATABASE rentals TO program;
 
 CREATE DATABASE payments;
 GRANT ALL PRIVILEGES ON DATABASE payments TO program;
+
+INSERT INTO public.cars(
+	id, car_uid, brand, model, registration_number, power, price, type, availability)
+	VALUES (1, '109b42f3-198d-4c89-9276-a7520a7120ab', 'Mercedes Benz', 'GLA 250', 'ЛО777Х799', 249, 3500, 'SEDAN', true);
